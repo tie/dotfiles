ed5b01fcbd889f2e57bff42c45559093ebe64352
Ivan Trubach <mr.trubach@icloud.com>
2018-02-18 05:42:51 +0300

    Disable solarized color scheme in xmonad/xmobar

--- xmonad/.config/xmobar/xmobar.hs
+++ xmonad/.config/xmobar/xmobar.hs
@@ -1,30 +1,49 @@
 Config { font = "xft:DejaVu Sans Mono:size=9"
        , additionalFonts = [ "xft:FontAwesome:size=8" ]
        , position = Top
-       , bgColor = "#002b36"
-       , fgColor = "#586e75"
        , commands =
          [ Run StdinReader
          , Run BatteryP [ "BAT0" ]
-                        [ "-t", "(<timeleft> left) (<watts>W) (<acstatus>) <left>%"
-                        , "-L", "10", "-H", "80", "-p", "3"
+                        [ "-f", "ADP1/online"
+                        , "-L", "15", "-l", "red"
+                        , "-H", "40", "-n", "pink"
+                        , "-S", "On", "-p", "3"
+
+                        , "-t", "<leftipat>"
                         , "--"
-                        ,  "-O", "Charging", "-i", "Idle", "-o", "Off"
-                        , "-L", "-15", "-H", "-5"
-                        , "-l", "red", "-m", "blue", "-h", "green"
+                        , "--idle-icon-pattern", "<left>"
+                        , "--off-icon-pattern", "<left> / <timeleft>"
+                        , "--on-icon-pattern", "<watts>W <left>"
+
                         ] 900
-         , Run Cpu [ "-L", "3", "-H", "50"
-                   , "--normal", "green", "--high", "red"
-                   , "-t", "<total>%"
-                   ] 10
+
+         , Run MultiCpu ["-t", "<autovbar>"
+                        , "-L", "30", "-n", "pink"
+                        , "-H", "60", "-h", "red"
+                        ] 10
+
+         -- "-S" did not work here last time I've tested it.
+         , Run ThermalZone 0 [ "-t", "PCH/<temp>°" ] 30
+         , Run ThermalZone 1 [ "-t", "BAT/<temp>°"] 30
+         , Run ThermalZone 2 [ "-t", "x86/<temp>°" ] 30
+
+           -- '-d' option was pushed to master just a few days ago...
+           -- I'm using pre-built binaries from Arch Linux, so I do not have this
+           -- feature working yet.
+         --, Run CpuFreq [ "-t", "<cpu0> <cpu1> <cpu1> <cpu1>"
+         --              , "-S", "On"
+         --              , "-d", "1"
+         --              ] 50
            -- Because I Don't Use Windows
          --, Run Weather "UUEE" [ "-t", "<skyCondition>" ] 36000
-         , Run Memory [ "-t", "<usedratio>%" ] 10
-         , Run Date "%a, %b %_d" "date" 864000 -- day
+         , Run Memory [ "-t", "<usedratio>"
+                      , "-S", "On", "-w", "3" ] 10
+
+         , Run Date "%a, %b %_d" "date" 864000 -- updates each day
          , Run Date "%I:%M %p" "time" 600
          ]
        , sepChar = "%"
        , alignSep = "{}"
        --, template = "%StdinReader% { %date% // %time% // %UUEE% } | %battery% <fn=1></fn> | %cpu% <fn=1></fn> | %memory% <fn=1></fn> "
-       , template = "%StdinReader% { %date% // %time% } | %battery% <fn=1></fn> | %cpu% <fn=1></fn> | %memory% <fn=1></fn> "
+       , template = "%StdinReader% { %date% // %time% } %multicpu% | %thermal0% %thermal1% %thermal2% <fn=1></fn> | %battery% <fn=1></fn> | %memory% <fn=1></fn> "
        }
--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -1,57 +1,52 @@
 import System.IO
 
 import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Layout.NoBorders(smartBorders)
-import XMonad.Hooks.EwmhDesktops
 import XMonad.Prompt
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks(avoidStruts)
+import XMonad.Layout.NoBorders(smartBorders)
 
 import Keys
+import EventHooks
 import Theme.Solarized.Dark as Colors
 
 --------------------------------------------------------------------------------
 
-xmobarCommand = "xmobar ~/.config/xmobar/xmobar.hs"
+toggleStrutsKey :: XConfig t -> (KeyMask, KeySym)
+toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b)
 
-themeConfig c = c { borderWidth        = 1
-                  , normalBorderColor  = Colors.base02
-                  , focusedBorderColor = Colors.base1
-                  }
+--------------------------------------------------------------------------------
 
-themePConfig c = c { position          = Top
-                   , bgColor           = Colors.base03
-                   , fgColor           = Colors.base0
-                   , fgHLight          = Colors.base00
-                   , bgHLight          = Colors.base02
-                   , borderColor       = Colors.base02
-                   , promptBorderWidth = 1
-                   }
+main =
+ xmonad =<< statusBar "xmobar $cfg/xmobar/xmobar.hs" pp toggleStrutsKey defaults
 
 pp = xmobarPP { ppHiddenNoWindows = const ""
-              , ppCurrent = xmobarColor Colors.base00 Colors.base02 . wrap " " " "
-              , ppVisible = xmobarColor Colors.base01 Colors.base03 . wrap " " " "
-              , ppHidden  = xmobarColor Colors.base01 Colors.base03 . wrap " " " "
+              , ppCurrent = wrap "[" "]"
+              , ppVisible = wrap " " " "
+              , ppHidden  = wrap " " " "
               , ppSep     = " | "
               , ppWsSep   = ""
                 -- TODO: Replace middle of the string with "..."
               , ppTitle  = shorten 32
               }
 
-prompt = themePConfig $
-        def { font   = "xft:DejaVu Sans Mono:size=9"
-              -- FIXME: compute height
-            , height = 38
-            , historySize = 0
-            }
+prompt = def { font              = "xft:DejaVu Sans Mono:size=9"
+             , historySize       = 0
+               -- FIXME: compute height
+             , height            = 38
+             , position          = Bottom
+             , promptBorderWidth = 1
+             }
 
-main = xmonad =<< statusBar xmobarCommand pp toggleStrutsKey defaults
-
-defaults = themeConfig $
-    def { terminal        = "uxterm"
-        , modMask         = mod4Mask
-          -- FIXME: redefine keys, layout and events handlers
-          --        because we do not want to depend on `def`
-        , keys            = Keys.promptKeys prompt <+> Keys.defaultKeys
-        , layoutHook      = smartBorders (layoutHook def)
-        , handleEventHook = handleEventHook def <+> fullscreenEventHook
-        }
+defaults = def { terminal           = "uxterm"
+               , modMask            = mod4Mask
+                 -- FIXME: redefine keys, layout and events handlers
+                 --        because we do not want to depend on `def`
+               , keys               = Keys.promptKeys prompt <+> Keys.defaultKeys
+               , layoutHook         = avoidStruts $ smartBorders $ layoutHook def
+               , handleEventHook    = handleEventHook def        <+>
+                                      fullscreenEventHook
+               , borderWidth        = 1
+               }
