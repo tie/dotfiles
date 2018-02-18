79fe5d76d6eda717abdc51dcc0b312e1e5547684
Ivan Trubach <mr.trubach@icloud.com>
2018-02-18 05:17:35 +0300

    Clean up xmonad key bindings

--- xmonad/.config/xmonad/lib/Keys.hs
+++ xmonad/.config/xmonad/lib/Keys.hs
@@ -1,7 +1,6 @@
 module Keys
   ( promptKeys
   , defaultKeys
-  , toggleStrutsKey
   ) where
 
 import System.Exit
@@ -18,9 +17,6 @@ import qualified XMonad.StackSet as W
 import Graphics.X11.Xlib
 import Graphics.X11.ExtraTypes.XF86
 
-toggleStrutsKey :: XConfig t -> (KeyMask, KeySym)
-toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b)
-
 promptKeys prompt conf@(XConfig { XMonad.modMask = modMask }) = M.fromList $
         [ ((modMask, xK_e), xmonadPrompt prompt)
         , ((modMask, xK_r), shellPrompt  prompt)
@@ -29,40 +25,55 @@ promptKeys prompt conf@(XConfig { XMonad.modMask = modMask }) = M.fromList $
 defaultKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 defaultKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- launching and killing programs
-    [ ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf) -- %! Launch terminal
-    , ((modMask .|. shiftMask, xK_c     ), kill) -- %! Close the focused window
-
-    , ((modMask,               xK_space ), sendMessage NextLayout) -- %! Rotate through the available layout algorithms
-    , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf) -- %!  Reset the layouts on the current workspace to default
-
-    , ((modMask,               xK_n     ), refresh) -- %! Resize viewed windows to the correct size
-
+      -- %! Launch terminal
+    [ ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+      -- %! Close the focused window
+    , ((modMask .|. shiftMask, xK_c     ), kill)
+      -- %! Rotate through the available layout algorithms
+    , ((modMask,               xK_space ), sendMessage NextLayout)
+      -- %!  Reset the layouts on the current workspace to default
+    , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+      -- %! Resize viewed windows to the correct size
+    , ((modMask,               xK_n     ), refresh)
     -- move focus up or down the window stack
-    , ((modMask,               xK_Tab   ), windows W.focusDown) -- %! Move focus to the next window
-    , ((modMask .|. shiftMask, xK_Tab   ), windows W.focusUp  ) -- %! Move focus to the previous window
-    , ((modMask,               xK_j     ), windows W.focusDown) -- %! Move focus to the next window
-    , ((modMask,               xK_k     ), windows W.focusUp  ) -- %! Move focus to the previous window
-    , ((modMask,               xK_m     ), windows W.focusMaster  ) -- %! Move focus to the master window
+      -- %! Move focus to the next window
+    , ((modMask,               xK_Tab   ), windows W.focusDown)
+      -- %! Move focus to the previous window
+    , ((modMask .|. shiftMask, xK_Tab   ), windows W.focusUp)
+      -- %! Move focus to the next window
+    , ((modMask,               xK_j     ), windows W.focusDown)
+      -- %! Move focus to the previous window
+    , ((modMask,               xK_k     ), windows W.focusUp)
+      -- %! Move focus to the master window
+    , ((modMask,               xK_m     ), windows W.focusMaster)
 
     -- modifying the window order
-    , ((modMask,               xK_Return), windows W.swapMaster) -- %! Swap the focused window and the master window
-    , ((modMask .|. shiftMask, xK_j     ), windows W.swapDown  ) -- %! Swap the focused window with the next window
-    , ((modMask .|. shiftMask, xK_k     ), windows W.swapUp    ) -- %! Swap the focused window with the previous window
+      -- %! Swap the focused window and the master window
+    , ((modMask,               xK_Return), windows W.swapMaster)
+      -- %! Swap the focused window with the next window
+    , ((modMask .|. shiftMask, xK_j     ), windows W.swapDown)
+      -- %! Swap the focused window with the previous window
+    , ((modMask .|. shiftMask, xK_k     ), windows W.swapUp)
 
     -- resizing the master/slave ratio
-    , ((modMask,               xK_h     ), sendMessage Shrink) -- %! Shrink the master area
-    , ((modMask,               xK_l     ), sendMessage Expand) -- %! Expand the master area
+      -- %! Shrink the master area
+    , ((modMask,               xK_h     ), sendMessage Shrink)
+      -- %! Expand the master area
+    , ((modMask,               xK_l     ), sendMessage Expand)
 
     -- floating layer support
-    , ((modMask,               xK_t     ), withFocused $ windows . W.sink) -- %! Push window back into tiling
+      -- %! Push window back into tiling
+    , ((modMask,               xK_t     ), withFocused $ windows . W.sink)
 
     -- increase or decrease number of windows in the master area
-    , ((modMask,               xK_comma ), sendMessage (IncMasterN ( 1))) -- %! Increment the number of windows in the master area
-    , ((modMask,               xK_period), sendMessage (IncMasterN (-1))) -- %! Deincrement the number of windows in the master area
+      -- %! Increment the number of windows in the master area
+    , ((modMask,               xK_comma ), sendMessage (IncMasterN ( 1)))
+      -- %! Deincrement the number of windows in the master area
+    , ((modMask,               xK_period), sendMessage (IncMasterN (-1)))
 
     -- quit, or restart
-    , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess)) -- %! Quit xmonad
-
+      -- %! Quit xmonad
+    , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
     ]
     ++
     -- mod-[1..9] %! Switch to workspace N
