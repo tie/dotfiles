14677b9111ef6401120f6ca34fa8d4e6e1a6edf6
Ivan Trubach <mr.trubach@icloud.com>
2018-02-21 14:17:33 +0300

    Clean up xmobar config

--- xmonad/.config/xmobar/xmobar.hs
+++ xmonad/.config/xmobar/xmobar.hs
@@ -7,13 +7,13 @@ Config { font = "xft:DejaVu Sans Mono:size=9"
                         [ "-f", "ADP1/online"
                         , "-L", "15", "-l", "red"
                         , "-H", "40", "-n", "pink"
-                        , "-S", "On", "-p", "3"
+                        , "-S", "On"
 
                         , "-t", "<leftipat>"
                         , "--"
                         , "--idle-icon-pattern", "<left>"
-                        , "--off-icon-pattern", "<left> / <timeleft>"
-                        , "--on-icon-pattern", "<watts>W <left>"
+                        , "--off-icon-pattern", "<timeleft> / <left>"
+                        , "--on-icon-pattern", "<watts> / <left>"
 
                         ] 900
 
@@ -27,23 +27,20 @@ Config { font = "xft:DejaVu Sans Mono:size=9"
          , Run ThermalZone 1 [ "-t", "BAT/<temp>°"] 30
          , Run ThermalZone 2 [ "-t", "x86/<temp>°" ] 30
 
-           -- '-d' option was pushed to master just a few days ago...
-           -- I'm using pre-built binaries from Arch Linux, so I do not have this
-           -- feature working yet.
-         --, Run CpuFreq [ "-t", "<cpu0> <cpu1> <cpu1> <cpu1>"
-         --              , "-S", "On"
-         --              , "-d", "1"
-         --              ] 50
-           -- Because I Don't Use Windows
-         --, Run Weather "UUEE" [ "-t", "<skyCondition>" ] 36000
-         , Run Memory [ "-t", "<usedratio>"
-                      , "-S", "On", "-w", "3" ] 10
+--       -- '-d' option was pushed to master just a few days ago...
+--       -- I'm using pre-built binaries from Arch Linux, so I do not have this
+--       -- feature working yet.
+--       , Run CpuFreq [ "-t", "<cpu0> <cpu1> <cpu1> <cpu1>"
+--                     , "-S", "On"
+--                     , "-d", "1"
+--                     ] 50
+--       , Run Weather "UUEE" [ "-t", "<skyCondition>" ] 36000
+         , Run Memory [ "-t", "<usedratio>", "-S", "On" ] 10
 
-         , Run Date "%a, %b %_d" "date" 864000 -- updates each day
+         , Run Date "%a, %b %_d" "date" 864000 -- update each day
          , Run Date "%I:%M %p" "time" 600
          ]
        , sepChar = "%"
        , alignSep = "{}"
-       --, template = "%StdinReader% { %date% // %time% // %UUEE% } | %battery% <fn=1></fn> | %cpu% <fn=1></fn> | %memory% <fn=1></fn> "
        , template = "%StdinReader% { %date% // %time% } %multicpu% | %thermal0% %thermal1% %thermal2% <fn=1></fn> | %battery% <fn=1></fn> | %memory% <fn=1></fn> "
        }
