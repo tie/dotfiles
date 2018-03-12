00b9a537dbe08cb54bffa83890b59b897284bc83
Ivan Trubach <mr.trubach@icloud.com>
2018-03-12 16:23:06 +0300

    Clean up xmobar config

--- xmonad/.config/xmobar/xmobar.hs
+++ xmonad/.config/xmobar/xmobar.hs
@@ -1,6 +1,6 @@
-Config { font = "xft:DejaVu Sans Mono:size=9"
-       , additionalFonts = [ "xft:FontAwesome:size=8" ]
-       , position = Top
+Config { position = Top
+       , font = "xft:Unifont:size=10"
+       , template = "%StdinReader% } %date% { %multicpu% %memory% | %thermal0% %thermal1% %thermal2% | %battery%"
        , commands =
          [ Run StdinReader
          , Run BatteryP [ "BAT0" ]
@@ -8,39 +8,32 @@ Config { font = "xft:DejaVu Sans Mono:size=9"
                         , "-L", "15", "-l", "red"
                         , "-H", "40", "-n", "pink"
                         , "-S", "On"
-
-                        , "-t", "<leftipat>"
+                        , "-t", "BAT:<leftipat>"
                         , "--"
                         , "--idle-icon-pattern", "<left>"
-                        , "--off-icon-pattern", "<timeleft> / <left>"
-                        , "--on-icon-pattern", "<watts> / <left>"
-
+                        , "--off-icon-pattern" , "<left> :: <timeleft>"
+                        , "--on-icon-pattern"  , "<left> :: <watts>"
                         ] 900
 
-         , Run MultiCpu ["-t", "<autovbar>"
+         , Run MultiCpu [ "-t", "<autovbar>"
                         , "-L", "30", "-n", "pink"
                         , "-H", "60", "-h", "red"
                         ] 10
 
-         -- "-S" did not work here last time I've tested it.
-         , Run ThermalZone 0 [ "-t", "PCH/<temp>°" ] 30
-         , Run ThermalZone 1 [ "-t", "BAT/<temp>°"] 30
-         , Run ThermalZone 2 [ "-t", "x86/<temp>°" ] 30
+         , Run ThermalZone 0 [ "-t", "PCH:<temp>℃" ] 30
+         , Run ThermalZone 1 [ "-t", "BAT:<temp>℃" ] 30
+         , Run ThermalZone 2 [ "-t", "X86:<temp>℃" ] 30
 
---       -- '-d' option was pushed to master just a few days ago...
+--       -- '-d' option was pushed to master just a few days ago.
 --       -- I'm using pre-built binaries from Arch Linux, so I do not have this
 --       -- feature working yet.
---       , Run CpuFreq [ "-t", "<cpu0> <cpu1> <cpu1> <cpu1>"
+--       , Run CpuFreq [ "-t", "<cpu0> <cpu1> <cpu2> <cpu3>"
 --                     , "-S", "On"
 --                     , "-d", "1"
 --                     ] 50
 --       , Run Weather "UUEE" [ "-t", "<skyCondition>" ] 36000
-         , Run Memory [ "-t", "<usedratio>", "-S", "On" ] 10
+         , Run Memory [ "-t", "RAM:<usedratio>", "-S", "On" ] 10
 
-         , Run Date "%a, %b %_d" "date" 864000 -- update each day
-         , Run Date "%I:%M %p" "time" 600
+         , Run Date "%a, %b %-d :: %I:%M %p" "date" 600
          ]
-       , sepChar = "%"
-       , alignSep = "{}"
-       , template = "%StdinReader% { %date% // %time% } %multicpu% | %thermal0% %thermal1% %thermal2% <fn=1></fn> | %battery% <fn=1></fn> | %memory% <fn=1></fn> "
        }
